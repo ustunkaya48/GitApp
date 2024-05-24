253e994e8e70ea9346a1293462564379fe652db6
Üstün Kaya <ustunkaya048@gmail.com>
2024-05-24 23:28:07 +0300

    v3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -8,5 +8,8 @@ public class Runner {
 
         //versiyon 2
         System.out.println("git commit -m\"\" ile versiyon oluşturulur");
+
+        //versiyon 3
+        System.out.println("Geçmişe yolculuk");
     }
 }
---
+++ src/main/java/Runner02.java
@@ -0,0 +1,7 @@
+public class Runner02 {
+    public static void main(String[] args) {
+
+        //versiyon 3
+        System.out.println("Hello");
+    }
+}
