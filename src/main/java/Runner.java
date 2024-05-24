4f757771828cfc0ed939a948ead7272786683d44
Üstün Kaya <ustunkaya048@gmail.com>
2024-05-24 22:23:44 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+
+    public static void main(String[] args) {
+
+        //versiyon 1
+        System.out.println("Hello GIT");
+        System.out.println("git add . : staging areaya alır");
+    }
+}
